8344b8788717927a5216768fde87da2bcd8a33ff
Sergio García <sergio4geeks@gmail.com>
2025-03-01 15:13:57 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let frase = "";
+
+  function x (arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+
+  frase += `${who[x(who)]} ${action[x(action)]} ${what[x(what)]} ${when[x(when)]}`;
+  document.getElementById("excuse").innerHTML = frase;
+
+  console.log("Mis datos", frase );
 };
